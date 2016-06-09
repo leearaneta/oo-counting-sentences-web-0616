6d8f8cdb6297be1a6227cef465af67d84b7237bb
leearaneta <lee.araneta@gmail.com>
2016-06-09 19:48:13 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    	true
+    else
+    	false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+    	true
+    else
+    	false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+    	true
+    else
+    	false
+    end
   end
 
   def count_sentences
-
+  	split_period = self.split(".")
+	split_question = split_period.map {|x| x.split("?")}.flatten
+	split_exclamation = split_question.map {|y| y.split("!")}.flatten
+	split_exclamation.delete("")
+  	return split_exclamation.size
   end
 end
